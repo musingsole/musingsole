5f37922f025b40835a6ed93e5a693d409dc23a06
musingsole <musingsole@gmail.com>
2020-11-29 22:45:22 -0500

    Scrolling and images

--- aframe.py
+++ aframe.py
@@ -2,27 +2,8 @@ import content
 import aframe_md_compiler
 
 
-template = """
-<html>
-<head>
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous">
-</script>
-</style>
-<title>Occultronic Provisions</title>
-<script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-</head>
-<body>
-    <a-scene background="color: #0A0A0A">
-        <a-entity camera="active: true" look-controls wasd-controls="fly: true; acceleration: 200" position="0 1.6 0"></a-entity>
-
-        {scene}
-    </a-scene>
-</body>
-</html>
-"""
+with open("aframe.html", "r") as fh:
+    template = fh.read()
 
 
 def get_aframe_client(event):
@@ -30,7 +11,7 @@ def get_aframe_client(event):
     try:
         entry = content.retrieve_entry("2020-10-07 Trivialized Nickel and Copper Plating")
         scene = entry.to_scene()
-        return 200, template.format(scene=scene)
+        return 200, aframe_md_compiler.template_keyword_replace(template, scene=scene)
     except Exception:
         from traceback import format_exc
         print(format_exc())
--- aframe_md_compiler.py
+++ aframe_md_compiler.py
@@ -1,76 +1,85 @@
 import re
 from functools import partial
-from LambdaPage import LambdaPage
 import content
 
 
-template = """
-<html>
-<head>
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous">
-</script>
-</style>
-<title>{title}</title>
-<script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-</head>
-<body>
-    <a-scene background="color: #0A0A0A">
-        <a-entity camera="active: true" look-controls wasd-controls="fly: true; acceleration: 200" position="0 1.6 0"></a-entity>
-        {scene}
-    </a-scene>
-</body>
-</html>
-"""
-
-
 class Parser:
+    patterns = {
+        "header": r"^(#+) (.*)",
+        "link": r"\[(.*)\]\((.*)\)",
+        "image": r"!\[(.*)\]\((.*)\)",
+        "paragraph": r".*"
+    }
+
     def __init__(self, pattern, func):
         self.pattern = pattern
         self.func = func
 
     def parse(self, chunk):
-        return re.match(self.pattern, chunk)
+        return re.search(self.pattern, chunk)
 
     def __call__(self, chunk):
-        match = re.match(self.pattern, chunk)
+        match = self.parse(chunk)
         if match:
             return self.func(chunk, match)
         else:
             return None, None
 
 
+def template_keyword_replace(template, **kwargs):
+    filled_template = template
+    for key, value in kwargs.items():
+        filled_template = filled_template.replace(f"{{{key}}}", str(value))
+    return filled_template
+
+
 def header(chunk, match):
     # header_level = len(match.group(1))
     print(f"HEADER CHUNK: {chunk}")
-    header_level = (1 / len(match.group(1))) * 10
+    header_level = len(match.group(1))
+    wrap_count = 25 + header_level * 10
+    height = 1.7 - 0.2 * header_level
     header_content = match.group(2)
 
     entity = """
-    <a-text position="{position}" value="{content}" color="green" align="center" width=5 height=2 wrap-count=15>
-        <a-entity geometry="primitive:box;width:5;height:2;depth:0.1" material="color: yellow" position="0 0 -0.1"></a-entity>
-    </a-text>"""
-    entity = partial(entity.format, content=header_content, header_level=header_level, hl2=header_level / 2 - 0.5)
-    # TODO: calculate height
-    return entity, (1, 2, 0.1)
+    <a-entity position="{position}" class="header_{header_level}">
+        <a-text value="{content}" color="green" align="center" width=10 wrap-count={wrap_count}></a-text>
+        <a-entity geometry="primitive:box;width:10;height:{height};depth:0.1" material="color: yellow" position="0 0 -0.1"></a-entity>
+    </a-entity>"""
+    entity = partial(template_keyword_replace, template=entity, content=header_content,
+                     header_level=header_level, wrap_count=wrap_count, height=height)
+
+    return entity, (1, height, 0.1)
+
+
+def image(chunk, match):
+    print(f"IMAGE CHUNK: {chunk}")
+    src_url = content.replace_asset_links(match.group(2))
+    entity = """
+    <a-entity position="{position}" class="paragraph">
+        <a-entity geometry="primitive:box;width:9.6;height:1;depth:0.1"
+         material="src: {src_url}; color: navy" position="0 0 -0.1"></a-entity>
+    </a-entity>}"""
+    return partial(template_keyword_replace, template=entity, src_url=src_url), (1, 1, 0.1)
 
 
 def paragraph(chunk, match):
     print(f"PARAGRAPH CHUNK: {chunk}")
-    height = len(chunk) / 40
+    height = max(len(chunk) / 580, 1)
     entity = """
-    <a-text position="{position}" value="{chunk}" color="white" align="left" width=5 height={height} wrap-count=50>
-        <a-entity geometry="primitive:box;width:6;height:{height};depth:0.1" material="color: navy" position="2.2 0 -0.05"></a-entity>
-        <a-entity geometry="primitive:box;width:5;height:{height};depth:0.1" material="color: yellow" position="0 0 -0.1"></a-entity>
-    </a-text>"""
-    return partial(entity.format, chunk=chunk, height=height), (1, height, 0.1)
+    <a-entity position="{position}" class="paragraph">
+        <a-text position="-4.6 0 0" value="{chunk}" color="white" align="left"
+         width=9.2 wrap-count=100></a-text>
+        <a-entity geometry="primitive:box;width:9.6;height:{height};depth:0.1"
+         material="color: navy" position="0 0 -0.1"></a-entity>
+    </a-entity>"""
+    return partial(template_keyword_replace, template=entity, chunk=chunk, height=height), (1, 1.25, 0.1)
 
 
 dispatch = [
-    Parser(r"^(#+) (.*)", header),
-    Parser(r"(.*)", paragraph)
+    Parser(Parser.patterns['image'], image),
+    Parser(Parser.patterns['header'], header),
+    Parser(Parser.patterns['paragraph'], paragraph)
 ]
 
 
@@ -81,19 +90,22 @@ def parse(chunk):
             return parsed, geo
 
 
-def parse_md(md, initial_position=[0, 0, 0]):
+def parse_md(md, initial_position=[0, 0, 0], hbuff=0.1):
     position = initial_position
     for chunk in md.split("\n"):
+        if chunk == "":
+            continue
         parsed, geo = parse(chunk)
+        position[1] -= (geo[1] + hbuff) / 2
         aframe_entity = parsed(position=", ".join([str(p) for p in position]))
-        position[1] -= geo[1]
+        position[1] -= (geo[1] + hbuff) / 2
         yield aframe_entity
 
 
 def entry_to_scene(entry):
     scene = list(parse_md(entry.body))
     scene = "\n".join(scene)
-    scene = f"<a-entity id='md' position='-2 3 -3'>{scene}</a-entity>"
+    scene = f"<a-entity id='md' position='0 3 -3'>{scene}</a-entity>"
     return scene
 
 
@@ -104,8 +116,6 @@ setattr(content.Entry, 'to_scene', entry_to_scene)
 #     pass
 
 
-# def image(chunk):
-#     pass
 
 
 # def bullet(chunk):
